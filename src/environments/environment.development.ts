412d18c5247cef4207539292c936b5a3e008322b
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-02-27 16:39:25 +0530

    change maximum error size

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-  domainUrl:'https://www.justice-love-peace.com',////production for peackeeper qrcode
-  tinyUrl :  'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
+  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
+  // domainUrl:'https://www.justice-love-peace.com',////production for peackeeper qrcode
+  // tinyUrl :  'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
 
-  // apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  // domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
-  // tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
+  apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
+  domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
+  tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
 
   // apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
   // domainUrl:'https://globaljusticeqc.cylsys.com',         //QC for peackeeper qrcode
