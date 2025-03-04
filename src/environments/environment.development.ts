0fd966cb0d971cbf4d2a052615a3a675889f2360
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-03-04 16:33:43 +0530

    change enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,21 +1,9 @@
 export const environment = {
   production: true,
 
-  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-  // domainUrl:'https://www.justice-love-peace.com',////production for peackeeper qrcode
-  // tinyUrl :  'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
-
-  apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
-  tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
-
-  // apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
-  // domainUrl:'https://globaljusticeqc.cylsys.com',         //QC for peackeeper qrcode
-  // tinyUrl : 'https://tinyurl.com/7yuvnmyj',               //tiny url for mediator page
-
-  apiMockUrl:'/api/v1',
-  app_env: 'production',
-
-  // tinyUrlLive :  'https://tinyurl.com/ys5z7n2z',
-  // tinyURLUAT :  'https://tinyurl.com/3322sj49'
+  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
+  domainUrl: 'https://www.justice-love-peace.com',////production for peackeeper qrcode
+  tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
+  apiMockUrl: '/api/v1',
+  app_env: 'production'
 };
