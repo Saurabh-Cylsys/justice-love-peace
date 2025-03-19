9e621487c4137e35a0ddb937656b9f287eb5d19a
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-03-19 20:32:45 +0530

    add live enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,9 @@
 export const environment = {
   production: true,
-
-  apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  domainUrl:'https://globaljusticeuat.cylsys.com',   ////production for peackeeper qrcode
-  // tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
-  tinyUrl: 'https://tinyurl.com/23ky8j3n',         //tiny url for https://globaljusticewf.cylsys.com/ page
+  
+  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
+  domainUrl: 'https://www.justice-love-peace.com',////production for peackeeper qrcode
+  tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
   apiMockUrl: '/api/v1',
   app_env: 'production',
   encryptionKey: 'np9dYaGAsDAswUhBUmrF5DsKDUXD5xlG4yn+6+wvaRoyP0UYgdOlSw5uGAa5iBNu',
