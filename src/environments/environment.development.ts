c76e4960fb3307d5ca3b83165297f2953b706c82
rupeshnarkar92 <rupesh.narkar@cylsys.com>
2025-03-22 18:11:09 +0530

    env updtaed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://devglobaljusticeapis.cylsys.com/api/v1',   //production
-  domainUrl: 'https://globaljusticeuat.cylsys.com',////production for peackeeper qrcode
-  tinyUrl: 'https://tinyurl.com/5n8txwht',         //tiny url for mediator page
+  apiUrl: 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
+  domainUrl: 'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
+  tinyUrl: 'https://tinyurl.com/5n8txwht',                    //tiny url for mediator page
   apiMockUrl: '/api/v1',
   app_env: 'development',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
