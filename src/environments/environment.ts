9ea1e0bf86ce0a9782d0c7ed4176d119192e7878
rupeshnarkar92 <rupesh.narkar@cylsys.com>
2025-03-27 01:11:44 +0530

    image updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,12 @@ export const environment = {
 
   apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
   domainUrl: 'https://www.justice-love-peace.com',////production for peackeeper qrcode
-  tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
+  // tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
   apiMockUrl: '/api/v1',
   app_env: 'development',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
   peacebookWebAppUrl: 'https://peacebook.justice-love-peace.com',
+  tinyUrl:'https://tinyurl.com/3322sj49'
 
 };
 
