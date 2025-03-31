7ef65180a5196a263cdba6de29a202f70ffed4e0
nadeem <nadeem@titan-consultancy.com>
2025-03-31 21:52:46 +0530

    fixed EVM

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@ export const environment = {
 
   apiUrl: 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
   domainUrl: 'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
-  // tinyUrl: 'https://tinyurl.com/5n8txwht',                    //tiny url for mediator page
+  tinyUrl: 'https://tinyurl.com/5n8txwht',                    //tiny url for mediator page
   apiMockUrl: '/api/v1',
   app_env: 'production',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
   peacebookWebAppUrl: 'https://peacekeeper-globaljusticeuat.cylsys.com',
-    tinyUrl : 'https://tinyurl.com/3322sj49',               ///for local testing only
+    // tinyUrl : 'https://tinyurl.com/3322sj49',               ///for local testing only
 };
 
