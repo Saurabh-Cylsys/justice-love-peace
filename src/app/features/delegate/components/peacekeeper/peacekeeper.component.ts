24c91a7e5f80f7d6f9abcc7fc0ad853559632353
rupeshnarkar92 <rupesh.narkar@cylsys.com>
2025-04-07 01:34:58 +0530

    code updated

--- src/app/features/delegate/components/peacekeeper/peacekeeper.component.ts
+++ src/app/features/delegate/components/peacekeeper/peacekeeper.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DelegateService } from '../../services/delegate.service';
+import { SharedService } from '../../../../shared/services/shared.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-peacekeeper',
@@ -11,17 +13,24 @@ export class PeacekeeperComponent {
 
   name: string | null = null;
 
-  constructor(private router: Router, private route: ActivatedRoute, private delegateService: DelegateService) { }
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private delegateService: DelegateService,
+              private sharedService: SharedService,
+              private ngxService :NgxUiLoaderService) { }
 
   ngOnInit(): void {
-    debugger;
+
     this.route.paramMap.subscribe(async params => {
       this.name = params.get('name'); // Retrieve the 'name' parameter from the URL
+
       if (this.name) {
         // Navigate to the desired route with the 'name' parameter
         let body = {
-          "coupon_code": this.name
+          "coupon_code": this.name.replace(/-/g, " "),
         }
+
+        this.ngxService.start();
         await this.delegateService.getAmbassadorURL(body).subscribe((resp: any) => {
           /**{
           "url": "https://www.justice-love-peace.com/delegate-registration?code=COIND-0000072-A",
@@ -29,8 +38,8 @@ export class PeacekeeperComponent {
           "error": false
       } */
 
-
           if (resp.success) {
+            this.ngxService.stop();
             let url = resp.url;
 
             // Use URLSearchParams to parse the query string
@@ -47,6 +56,10 @@ export class PeacekeeperComponent {
             }
           }
 
+        },(err)=>{
+          console.log(err);
+          // Handle the error if needed
+          this.sharedService.ToastPopup(err.message, '', 'error');
         });
       }
     });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
   tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
   apiMockUrl: '/api/v1',
-  app_env: 'production',
+  app_env: 'development',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
   peacebookWebAppUrl: 'https://peacekeeper-globaljusticeuat.cylsys.com',
     // tinyUrl : 'https://tinyurl.com/3322sj49',               ///for local testing only
