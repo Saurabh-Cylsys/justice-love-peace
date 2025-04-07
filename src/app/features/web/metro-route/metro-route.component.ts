10d0db8cd6af77f990e35fbbc9c0afe5434bfdb4
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-04-07 14:44:06 +0530

    add metro route component

---
+++ src/app/features/web/metro-route/metro-route.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metro-route',
+  templateUrl: './metro-route.component.html',
+  styleUrl: './metro-route.component.css'
+})
+export class MetroRouteComponent {
+
+}
