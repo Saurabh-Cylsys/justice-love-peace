b00f8ba89a0eb7ff20614acf193a47fdd480ba25
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-04-14 02:04:41 +0530

    work on stay tuned page

--- src/app/features/web/stay-tuned/stay-tuned.component.ts
+++ src/app/features/web/stay-tuned/stay-tuned.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stay-tuned',
   templateUrl: './stay-tuned.component.html',
-  styleUrl: './stay-tuned.component.css'
+  styleUrl: './stay-tuned.component.scss'
 })
 export class StayTunedComponent {
 
