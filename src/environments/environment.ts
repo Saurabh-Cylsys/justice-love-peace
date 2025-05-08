8cecf5ddaa02b9952b9e65a79041a262fb4f156a
nadeem <nadeem@titan-consultancy.com>
2025-05-08 09:50:04 +0530

    change peacebook url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   apiMockUrl: '/api/v1',
   app_env: 'development',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
-  peacebookWebAppUrl: 'http://i-am-peacekeeper.com',
+  peacebookWebAppUrl: 'https://i-am-peacekeeper.com',
     // tinyUrl : 'https://tinyurl.com/3322sj49',               ///for local testing only
 
 };
