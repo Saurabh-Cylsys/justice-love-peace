89359b20fcd87b90cd4143701527e9320768d16d
nadeem <nadeem@titan-consultancy.com>
2025-05-10 12:58:14 +0530

    set env for LIVE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,13 @@
 export const environment = {
   production: false,
   
-  apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
-  tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
+  apiUrl : 'https://api.justice-love-peace.com/api/v1', //production
+  domainUrl:'https://www.justice-love-peace.com', //development for peackeeper qrcode
+  tinyUrl :'https://tinyurl.com/ys5z7n2z', //tiny url for mediator page
   apiMockUrl: '/api/v1',
-  app_env: 'production',
+  app_env: 'development',
   encryptionKey: '6b5872594167471930cfe5c0b99cb6bfafd7b1601ee9f439359a7dde010a5ce9',
-  peacebookWebAppUrl: 'https://peacekeeper-globaljusticeuat.cylsys.com',
+  peacebookWebAppUrl: 'https://i-am-peacekeeper.com',
     // tinyUrl : 'https://tinyurl.com/3322sj49',               ///for local testing only
 
 };
